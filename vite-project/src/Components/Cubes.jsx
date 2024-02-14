a41ee5bf158871ca1fb0f23e2588f1b8d5ed9f82
Nicopobletee <149529377+Nplandry@users.noreply.github.com>
2024-02-13 23:44:52 -0300

    Update Cubes.jsx

--- vite-project/src/Components/Cubes.jsx
+++ vite-project/src/Components/Cubes.jsx
@@ -3,9 +3,10 @@ import { Cube } from './Cube.jsx'
 
 export const Cubes = () => {
     const [cubes] = useStore(state => [state.cubes])
-
+    //Recorre los cubos para obtener la id, posicion y las texturas
     return cubes.map(({ id, pos, texture }) => {
         return (
+            //Retornar cubo...
         <Cube 
         key={id}
         id={id}
